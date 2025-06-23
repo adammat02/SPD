a2e489d4214a4b740f1983a9b787bfba133401b0
Adam Matusiak <275460@student.pwr.edu.pl>
2025-06-23 15:51:15 +0200

    taillard tests

--- lab03/main.cpp
+++ lab03/main.cpp
@@ -6,6 +6,9 @@
 
 int main()
 {
+
+    // Czesc kodu dla wygenerowanych testow
+
     std::vector<std::string> files = {
         "test3x2.dat",
         "test4x2.dat",
@@ -53,6 +56,50 @@ int main()
 
         std::cout << "----------------------------------------" << std::endl;
     }
+    std::cout << std::endl;
+    std::cout << std::endl;
+    std::cout << "################## Taillard #######################" << std::endl;
+    std::cout << std::endl;
+    std::cout << std::endl;
+    // Czesc kodu dla plikow Taillard, tylko neh i fneh
+
+    files = {
+        "test20x5.dat",
+        "test20x10.dat",
+        "test20x20.dat",
+        "test50x5.dat",
+        "test50x10.dat",
+        "test50x20.dat",
+
+        // Dodaj więcej plików, jeśli istnieją
+    };
+
+    std::vector<int> true_values = {1, 1, 1, 1, 1, 1};
+
+    for (int i = 0; i < files.size(); ++i)
+    {
+        std::cout << "Processing file: " << files[i] << std::endl;
+
+        problem p(files[i]);
+
+        auto start = std::chrono::high_resolution_clock::now();
+        int neh = p.NEH();
+        auto end = std::chrono::high_resolution_clock::now();
+        auto neh_time = std::chrono::duration<double, std::milli>(end - start).count();
+
+        std::cout << "neh: " << neh << ", time: " << neh_time << ", error: " << static_cast<double>(std::abs(neh - true_values[i])) / true_values[i] * 100 << "%" << std::endl;
+
+        start = std::chrono::high_resolution_clock::now();
+        int fneh = p.NEH_2();
+        end = std::chrono::high_resolution_clock::now();
+        auto fneh_time = std::chrono::duration<double, std::milli>(end - start).count();
+
+        std::cout << "fneh: " << fneh << ", time: " << fneh_time << ", error: " << static_cast<double>(std::abs(fneh - true_values[i])) / true_values[i] * 100 << "%" << std::endl;
+
+        std::cout << "----------------------------------------" << std::endl;
+    }
+
+
 
     return 0;
 }
