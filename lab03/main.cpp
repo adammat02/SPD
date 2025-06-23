295740b10b04ebaf56a547c650afa9cd87accc8b
Adam Matusiak <275460@student.pwr.edu.pl>
2025-06-23 15:59:41 +0200

    taillard true values

--- lab03/main.cpp
+++ lab03/main.cpp
@@ -20,47 +20,48 @@ int main()
         // Dodaj więcej plików, jeśli istnieją
     };
 
-    for (const auto &file : files)
-    {
-        std::cout << "Processing file: " << file << std::endl;
-
-        problem p(file);
-
-        auto start = std::chrono::high_resolution_clock::now();
-        int p_zup = p.przegladZupelny();
-        auto end = std::chrono::high_resolution_clock::now();
-        auto p_zup_time = std::chrono::duration<double, std::milli>(end - start).count();
-
-        std::cout << "p_zup: " << p_zup << " time: " << p_zup_time << std::endl;
-
-        start = std::chrono::high_resolution_clock::now();
-        int neh = p.NEH();
-        end = std::chrono::high_resolution_clock::now();
-        auto neh_time = std::chrono::duration<double, std::milli>(end - start).count();
-
-        std::cout << "neh: " << neh << ", time: " << neh_time << ", error: " << static_cast<double>(std::abs(neh - p_zup)) / p_zup * 100 << "%" << std::endl;
-
-        start = std::chrono::high_resolution_clock::now();
-        int fneh = p.NEH_2();
-        end = std::chrono::high_resolution_clock::now();
-        auto fneh_time = std::chrono::duration<double, std::milli>(end - start).count();
-
-        std::cout << "fneh: " << fneh << ", time: " << fneh_time << ", error: " << static_cast<double>(std::abs(fneh - p_zup)) / p_zup * 100 << "%" << std::endl;
-
-        start = std::chrono::high_resolution_clock::now();
-        int jh = p.Johnson();
-        end = std::chrono::high_resolution_clock::now();
-        auto jh_time = std::chrono::duration<double, std::milli>(end - start).count();
-
-        std::cout << "jh: " << jh << ", time: " << jh_time << ", error: " << static_cast<double>(std::abs(jh - p_zup)) / p_zup * 100 << "%" << std::endl;
-
-        std::cout << "----------------------------------------" << std::endl;
-    }
+    // for (const auto &file : files)
+    // {
+    //     std::cout << "Processing file: " << file << std::endl;
+    //
+    //     problem p(file);
+    //
+    //     auto start = std::chrono::high_resolution_clock::now();
+    //     int p_zup = p.przegladZupelny();
+    //     auto end = std::chrono::high_resolution_clock::now();
+    //     auto p_zup_time = std::chrono::duration<double, std::milli>(end - start).count();
+    //
+    //     std::cout << "p_zup: " << p_zup << " time: " << p_zup_time << std::endl;
+    //
+    //     start = std::chrono::high_resolution_clock::now();
+    //     int neh = p.NEH();
+    //     end = std::chrono::high_resolution_clock::now();
+    //     auto neh_time = std::chrono::duration<double, std::milli>(end - start).count();
+    //
+    //     std::cout << "neh: " << neh << ", time: " << neh_time << ", error: " << static_cast<double>(std::abs(neh - p_zup)) / p_zup * 100 << "%" << std::endl;
+    //
+    //     start = std::chrono::high_resolution_clock::now();
+    //     int fneh = p.NEH_2();
+    //     end = std::chrono::high_resolution_clock::now();
+    //     auto fneh_time = std::chrono::duration<double, std::milli>(end - start).count();
+    //
+    //     std::cout << "fneh: " << fneh << ", time: " << fneh_time << ", error: " << static_cast<double>(std::abs(fneh - p_zup)) / p_zup * 100 << "%" << std::endl;
+    //
+    //     start = std::chrono::high_resolution_clock::now();
+    //     int jh = p.Johnson();
+    //     end = std::chrono::high_resolution_clock::now();
+    //     auto jh_time = std::chrono::duration<double, std::milli>(end - start).count();
+    //
+    //     std::cout << "jh: " << jh << ", time: " << jh_time << ", error: " << static_cast<double>(std::abs(jh - p_zup)) / p_zup * 100 << "%" << std::endl;
+    //
+    //     std::cout << "----------------------------------------" << std::endl;
+    // }
     std::cout << std::endl;
     std::cout << std::endl;
     std::cout << "################## Taillard #######################" << std::endl;
     std::cout << std::endl;
     std::cout << std::endl;
+
     // Czesc kodu dla plikow Taillard, tylko neh i fneh
 
     files = {
@@ -74,7 +75,7 @@ int main()
         // Dodaj więcej plików, jeśli istnieją
     };
 
-    std::vector<int> true_values = {1, 1, 1, 1, 1, 1};
+    std::vector<int> true_values = {1278, 1582, 2297, 2724, 3037, 3886};
 
     for (int i = 0; i < files.size(); ++i)
     {
